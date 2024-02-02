15b3f9ab6c0247eb4fa4d19d113687feb5bb5f77
Pheakboy <128297342+Pheakboy@users.noreply.github.com>
2024-02-02 11:05:33 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
 
 
-// // const Person = (props) => {
-// //   return (
-// //     <>
-// //       <h2>Name : {props.name} </h2>
-// //       <h2>Age : {props.age} </h2>
-// //       <h2>Gender: {props.gender} </h2>
-// //     </>
-// //   )
-// // }
-//   //  {/* <Person name= {'Jonh'} age= {'20'} gender={'Male'} />
-//   //     <Person name= {'son'} age= {'25'} gender={'Female'} /> */}
-// import {useState, useEffect} from 'react';
-
-// const App = () => {
-//   const [counter, setCounter] = useState(0);
-
-//   useEffect(() => {
-//     alert("You hvae changed your counter to " + counter)
-//   },[counter]);
-
-//   return (
-//     <div className="App">
-//       <button onClick={() => setCounter((prevCount) => prevCount-1 )}>-</button>
-//       <h2>{counter}</h2>
-//       <button onClick={() =>setCounter((prevCount) =>prevCount+1 )}>+</button>
-//        <button onClick={() => setCounter((prevCount) => prevCount =0)}>Reset</button>
-
-//     </div>
-//   );
-// }
-// export default App;
-
-
 import { useEffect, useState } from "react";
 import './App.css'
 import MovieCard from "./MovieCard";
 import SearchIcon from './search.svg'
 const API_URL = 'http://www.omdbapi.com?apikey=766fbd9a';
-
-const movie1 = {
-  "Title": "Spiderman",
-  "Year": "2010",
-  "imdbID": "tt1785572",
-  "Type": "movie",
-  "Poster": "N/A"
-}
-
-
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState([]);
